cb1f89c323787bb2c31b8544fa74cf24e1734e4c
Will  Kelly <willkelly0@gmail.com>
2022-02-28 13:42:57 -0500

    edit to test husky

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex h-screen flex-col">
       <Head>
-        <title>Web3 Next-Boilerplate</title>
+        <title>Web3 Next-Boilerplate Mono</title>
         <meta name="description" content="Boilerplate for Web3 dApp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
