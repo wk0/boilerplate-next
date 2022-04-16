be1f7d85236b0446c9f3a5638a9c1f80264054ae
Will Liu <willliuwillliu@gmail.com>
2022-04-16 17:29:51 -0400

    Updated button text and styling

--- components/Web3Button.tsx
+++ components/Web3Button.tsx
@@ -1,35 +1,35 @@
 import React from 'react'
-import { useWeb3Context } from '../context/'
-
-interface ConnectProps {
-  connect: (() => Promise<void>) | null
-}
-const ConnectButton = ({ connect }: ConnectProps) => {
-  return connect ? (
-    <button onClick={connect}>Connect</button>
-  ) : (
-    <button>Loading...</button>
-  )
-}
-
-interface DisconnectProps {
-  disconnect: (() => Promise<void>) | null
-}
-
-const DisconnectButton = ({ disconnect }: DisconnectProps) => {
-  return disconnect ? (
-    <button onClick={disconnect}>Disconnect</button>
-  ) : (
-    <button>Loading...</button>
-  )
-}
+import { useWeb3Context } from '../context'
 
 export function Web3Button() {
-  const { web3Provider, connect, disconnect } = useWeb3Context()
+  const { web3Provider, connect, disconnect, address } = useWeb3Context()
 
-  return web3Provider ? (
-    <DisconnectButton disconnect={disconnect} />
-  ) : (
-    <ConnectButton connect={connect} />
-  )
+  interface ButtonProps {
+    callback: (() => Promise<void>) | null
+  }
+
+  const Button = ({ callback }: ButtonProps) => {
+    return callback ? (
+      <button
+        className="group m-4 rounded-lg bg-black py-2 px-4 font-bold text-white hover:bg-gray-700"
+        onClick={callback}
+      >
+        {web3Provider ? (
+          <div className="">
+            <span className="group-hover:hidden">{`${address?.substring(
+              0,
+              5
+            )}...${address?.substring(address.length - 4)}`}</span>
+            <span className="hidden group-hover:block">Disconnect?</span>
+          </div>
+        ) : (
+          'Connect Wallet'
+        )}
+      </button>
+    ) : (
+      <button>Loading...</button>
+    )
+  }
+
+  return <Button callback={web3Provider ? disconnect : connect} />
 }
