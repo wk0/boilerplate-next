95fc660031d5f9b5dbb5444b0014433dbd3f69a9
Will Liu <willliuwillliu@gmail.com>
2022-04-16 15:31:00 -0400

    Reverting to previous button

--- components/Web3Button.tsx
+++ components/Web3Button.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
-import { useWeb3Context } from '../context'
+import { useWeb3Context } from '../context/'
+
+interface ConnectProps {
+  connect: (() => Promise<void>) | null
+}
+const ConnectButton = ({ connect }: ConnectProps) => {
+  return connect ? (
+    <button onClick={connect}>Connect</button>
+  ) : (
+    <button>Loading...</button>
+  )
+}
+
+interface DisconnectProps {
+  disconnect: (() => Promise<void>) | null
+}
+
+const DisconnectButton = ({ disconnect }: DisconnectProps) => {
+  return disconnect ? (
+    <button onClick={disconnect}>Disconnect</button>
+  ) : (
+    <button>Loading...</button>
+  )
+}
 
 export function Web3Button() {
-  const { web3Provider, connect, disconnect, address } = useWeb3Context()
+  const { web3Provider, connect, disconnect } = useWeb3Context()
 
-  return (
-    <button
-      className="m-4 rounded-lg bg-black py-2 px-4 font-bold text-white"
-      onClick={() => {
-        if (web3Provider) {
-          disconnect()
-        } else {
-          connect()
-        }
-      }}
-    >
-      {web3Provider ? `${address}` : 'Connect'}
-    </button>
+  return web3Provider ? (
+    <DisconnectButton disconnect={disconnect} />
+  ) : (
+    <ConnectButton connect={connect} />
   )
 }
