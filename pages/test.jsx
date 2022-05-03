4fda12b8c3311ee0ea66f99e6e29b5434855017b
Will Liu <willliuwillliu@gmail.com>
2022-05-02 23:26:21 -0400

    Removed dummy file

--- pages/test.jsx
+++
@@ -1,8 +0,0 @@
-import { ethers } from 'ethers'
-export const Test = ({}) => {
-  const a = ethers.utils.parseUnits('2', 'ether')
-  const b = ethers.utils.parseUnits('2', 'ether')
-  // console.log('a', a)
-  // console.log(a.mul(b))
-  return <div></div>
-}
