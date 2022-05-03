cd0c2f25914e58308e74075dd2ba98154d0857af
Will Liu <willliuwillliu@gmail.com>
2022-05-03 11:55:08 -0400

    test

--- pages/api/claimproof.ts
+++ pages/api/claimproof.ts
@@ -6,19 +6,14 @@ import mintPhases from '../../data/phases'
 
 function getUserMintDetails(address: string) {
   // find the phase in mintPhases that contains the address
-  // const usersPhase = mintPhases.find((phase) => {
-  //   return phase.allowedMints[address] != null
-  // })
+  const usersPhase = mintPhases.find((phase) => {
+    return phase.allowedMints[address] != null
+  })
   // // returns phase name, max mint, and mint price
-  // return {
-  //   userPhase: usersPhase?.name || 'No Phase',
-  //   allowedMints: usersPhase?.allowedMints[address] || 0,
-  //   pricePerToken: usersPhase?.pricePerToken || 0,
-  // }
   return {
-    userPhase: 'TEAMS',
-    allowedMints: 10,
-    pricePerToken: 0,
+    userPhase: usersPhase?.name || 'No Phase',
+    allowedMints: usersPhase?.allowedMints[address] || 0,
+    pricePerToken: usersPhase?.pricePerToken || 0,
   }
 }
 
