c75705920cf4fc084410c7fab19edada69c67ce2
pfed-prog <fedorovn19@gmail.com>
2022-07-23 19:01:03 +0200

    deleted about

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,21 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3ContextProvider } from '../context'
-import { ToastContainer } from 'react-toastify'
-
-import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Web3ContextProvider>
-      <>
-        <Component {...pageProps} />
-        <ToastContainer
-          hideProgressBar
-          position="bottom-right"
-          autoClose={2000}
-        />
-      </>
+      <Component {...pageProps} />
     </Web3ContextProvider>
   )
 }
--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,12 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import {
-  Web3Button,
-  Web3Address,
-  Web3Balance,
-  Web3Network,
-} from '../components/'
+import { Web3Address, Web3Balance, Web3Network } from '../components/'
 
 const Home: NextPage = () => {
   return (
@@ -16,13 +10,6 @@ const Home: NextPage = () => {
         <meta name="description" content="Boilerplate for Web3 dApp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <nav className="flex flex-row justify-between p-4">
-        <Link href="/about">
-          <a className="text-lg font-light">About</a>
-        </Link>
-        <Web3Button />
-      </nav>
 
       <main className="grow p-8 text-center">
         <h1 className="pb-8 text-4xl font-bold">Home Page</h1>
